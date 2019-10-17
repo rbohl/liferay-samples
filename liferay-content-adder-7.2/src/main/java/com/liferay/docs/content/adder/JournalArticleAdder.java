4ce0c8f6a96bc2e7c86f4ed873a936c9665dade4
Russell Bohl <russell.bohl@liferay.com>
2019-10-17 17:30:34 -0400

    cleaning up a bit

--- liferay-content-adder-7.2/src/main/java/com/liferay/docs/content/adder/JournalArticleAdder.java
+++ liferay-content-adder-7.2/src/main/java/com/liferay/docs/content/adder/JournalArticleAdder.java
@@ -1,6 +1,7 @@
 package com.liferay.docs.content.adder;
 
 import java.io.File;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -12,6 +13,7 @@ import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.model.JournalArticleConstants;
 import com.liferay.journal.model.JournalFolderConstants;
 import com.liferay.journal.service.JournalArticleLocalService;
@@ -23,14 +25,18 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 @Component(configurationPid = "com.liferay.docs.content.adder.JournalArticleAdderConfiguration")
 public class JournalArticleAdder {
@@ -55,25 +61,39 @@ public class JournalArticleAdder {
 		List<User> adminUsers = UserLocalServiceUtil.getRoleUsers(adminRole.getRoleId());
 		long userId = adminUsers.get(0).getUserId();
 
-        String articleURL = StringPool.BLANK;
-        Map<String,byte[]> images = null;
-        File smallImageFile = null;
-        String smallImageURL = StringPool.BLANK;
-        boolean smallImage = false;
-        boolean indexable = true;
-        boolean neverExpire = true;
-        boolean neverReview = true;
-        int date = 0;
-        String layoutUuid = StringPool.BLANK;
-        long classNameId = JournalArticleConstants.CLASSNAME_ID_DEFAULT;
-        long classPK = 0;
+		String portletId = PortletProviderUtil.getPortletId(
+			JournalArticle.class.getName(), PortletProvider.Action.EDIT);
+
+        String articleURL = _portal.getControlPanelFullURL(
+            groupId, portletId, null);
+
+		String namespace = _portal.getPortletNamespace(portletId);
+
         String articleId = StringPool.BLANK;
-        boolean autoArticleId = true;
-        double version = 1;
+
+		articleURL = _http.addParameter(
+			articleURL, namespace + "groupId", groupId);
+		articleURL = _http.addParameter(
+			articleURL, namespace + "folderId", folderId);
+		articleURL = _http.addParameter(
+			articleURL, namespace + "articleId", articleId);
 
 		ServiceContext serviceContext = new ServiceContext();
 		serviceContext.setScopeGroupId(groupId);
+		serviceContext.isAddGroupPermissions();
+		serviceContext.isAddGuestPermissions();
+		serviceContext.setAssetCategoryIds(null);
+		serviceContext.setAssetEntryVisible(true);
+		serviceContext.setAssetLinkEntryIds(null);
+		serviceContext.setAssetPriority(0.0);
+		serviceContext.setAssetTagNames(null);
 
+        serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+        serviceContext.setIndexingEnabled(true);
+        Date now = new Date();
+        serviceContext.setModifiedDate(now);
+        serviceContext.setCreateDate(now);
+        
 		String[] titles = _journalArticleConfiguration.titles();
 		String[] contents = _journalArticleConfiguration.contents();
 		String[] descriptions = _journalArticleConfiguration.descriptions();
@@ -88,6 +108,7 @@ public class JournalArticleAdder {
 			
 			Map<Locale, String> friendlyURLMap = new HashMap<>();
 			friendlyURLMap.put(LocaleUtil.US, titles[i]);
+			//friendlyURLMap = null;
 
 			Map<Locale, String> titleMap = new HashMap<>();
 			titleMap.put(LocaleUtil.US, titles[i]);
@@ -106,12 +127,18 @@ public class JournalArticleAdder {
 			String content = "<?xml version=\"1.0\"?><root><dynamic-element name=\"content\" type=\"text_area\" index-type=\"text\" instance-id=\"qmln\"><dynamic-content language-id=\"en_US\"><![CDATA[<p>"
 					+ contents[i] + "</p>]]></dynamic-content></dynamic-element></root>";
 
-			_jals.addArticle(userId, groupId, folderId, classNameId, classPK, articleId, autoArticleId, version,
-					titleMap, descriptionMap, friendlyURLMap, content, "BASIC-WEB-CONTENT", "BASIC-WEB-CONTENT",
-					layoutUuid, date, date, date, date, date, date, date, date, date, date, neverExpire, date, date,
-					date, date, date, neverReview, indexable, smallImage, smallImageURL, smallImageFile, images,
-					articleURL, serviceContext);
+			/*
+			 * _jals.addArticle(userId, groupId, folderId, classNameId, classPK, articleId,
+			 * autoArticleId, version, titleMap, descriptionMap, friendlyURLMap, content,
+			 * "BASIC-WEB-CONTENT", "BASIC-WEB-CONTENT", layoutUuid, displayDateMonth,
+			 * displayDateDay, displayDateYear, displayDateHour, displayDateMinute, date,
+			 * date, date, date, date, neverExpire, date, date, date, date, date,
+			 * neverReview, indexable, smallImage, smallImageURL, smallImageFile, images,
+			 * articleURL, serviceContext);
+			 */
 
+			_jals.addArticle(userId, groupId, folderId, titleMap, descriptionMap, content, "BASIC-WEB-CONTENT",
+					"BASIC-WEB-CONTENT", serviceContext);
 		}
 	}
 
@@ -132,6 +159,9 @@ public class JournalArticleAdder {
 
 	@Reference
 	Portal _portal;
+
+	@Reference
+	private Http _http;
 
 	@Reference
 	private GroupLocalService _groupLocalService;
