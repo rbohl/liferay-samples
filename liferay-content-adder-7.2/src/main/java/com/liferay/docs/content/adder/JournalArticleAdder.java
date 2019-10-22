c87e38414cf51a3646ba4938a5556a6b3e12f7aa
Russell Bohl <russell.bohl@liferay.com>
2019-10-21 22:38:49 -0400

    fixing the content and other edits

--- liferay-content-adder-7.2/src/main/java/com/liferay/docs/content/adder/JournalArticleAdder.java
+++ liferay-content-adder-7.2/src/main/java/com/liferay/docs/content/adder/JournalArticleAdder.java
@@ -1,6 +1,5 @@
 package com.liferay.docs.content.adder;
 
-import java.io.File;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -11,16 +10,22 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.model.JournalArticleConstants;
 import com.liferay.journal.model.JournalFolderConstants;
 import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.service.JournalFolderLocalService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Role;
@@ -37,6 +42,9 @@ import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.xml.Document;
+import com.liferay.portal.kernel.xml.Element;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 
 @Component(configurationPid = "com.liferay.docs.content.adder.JournalArticleAdderConfiguration")
 public class JournalArticleAdder {
@@ -61,71 +69,82 @@ public class JournalArticleAdder {
 		List<User> adminUsers = UserLocalServiceUtil.getRoleUsers(adminRole.getRoleId());
 		long userId = adminUsers.get(0).getUserId();
 
-		String portletId = PortletProviderUtil.getPortletId(
-			JournalArticle.class.getName(), PortletProvider.Action.EDIT);
+		String portletId = PortletProviderUtil.getPortletId(JournalArticle.class.getName(),
+				PortletProvider.Action.EDIT);
 
-        String articleURL = _portal.getControlPanelFullURL(
-            groupId, portletId, null);
+		String articleURL = _portal.getControlPanelFullURL(groupId, portletId, null);
 
 		String namespace = _portal.getPortletNamespace(portletId);
 
-        String articleId = StringPool.BLANK;
+		String articleId = StringPool.BLANK;
 
-		articleURL = _http.addParameter(
-			articleURL, namespace + "groupId", groupId);
-		articleURL = _http.addParameter(
-			articleURL, namespace + "folderId", folderId);
-		articleURL = _http.addParameter(
-			articleURL, namespace + "articleId", articleId);
+		articleURL = _http.addParameter(articleURL, namespace + "groupId", groupId);
+		articleURL = _http.addParameter(articleURL, namespace + "folderId", folderId);
+		articleURL = _http.addParameter(articleURL, namespace + "articleId", articleId);
 
 		ServiceContext serviceContext = new ServiceContext();
-		serviceContext.setScopeGroupId(groupId);
-		serviceContext.isAddGroupPermissions();
-		serviceContext.isAddGuestPermissions();
-		serviceContext.setAssetCategoryIds(null);
-		serviceContext.setAssetEntryVisible(true);
-		serviceContext.setAssetLinkEntryIds(null);
-		serviceContext.setAssetPriority(0.0);
-		serviceContext.setAssetTagNames(null);
 
-        serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
-        serviceContext.setIndexingEnabled(true);
-        Date now = new Date();
-        serviceContext.setModifiedDate(now);
-        serviceContext.setCreateDate(now);
-        
+		long[] assetCategoryIds = new long[1];
+
+		if (_assetVocabularyLocalService.getAssetVocabulariesCount() == 0) {
+			AssetVocabulary av = _assetVocabularyLocalService.addVocabulary(userId, groupId, "space", serviceContext);
+			AssetCategory ac = _assetCategoryLocalService.addCategory(userId, groupId, "lunar", av.getVocabularyId(),
+					serviceContext);
+
+			assetCategoryIds = new long[(int) ac.getCategoryId()];		  
+		} else {
+			List<AssetCategory> acs = _assetCategoryLocalService.getCategories();
+			AssetCategory ac = acs.get(0);
+			assetCategoryIds = new long[(int) ac.getCategoryId()];		  
+
+		}
+		 
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+		serviceContext.setScopeGroupId(groupId);
+		
+		  serviceContext.setAssetCategoryIds(assetCategoryIds);
+		  serviceContext.setAssetEntryVisible(true);
+		  serviceContext.setAssetLinkEntryIds(null);
+		  serviceContext.setAssetPriority(1);
+		  serviceContext.setAssetTagNames(null);
+		 
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+		serviceContext.setIndexingEnabled(true);
+		Date now = new Date();
+		serviceContext.setModifiedDate(now);
+		serviceContext.setCreateDate(now);
+
 		String[] titles = _journalArticleConfiguration.titles();
 		String[] contents = _journalArticleConfiguration.contents();
 		String[] descriptions = _journalArticleConfiguration.descriptions();
 
 		if (titles.length == 0 | titles.length != contents.length) {
 
-			// log something here
+			_log.error(
+					"Make sure there are titles in the confiugration, and that the number of titles and content are equal.");
 			return;
 		}
 
 		for (int i = 0; i < titles.length; i++) {
-			
+
 			Map<Locale, String> friendlyURLMap = new HashMap<>();
 			friendlyURLMap.put(LocaleUtil.US, titles[i]);
-			//friendlyURLMap = null;
+
+			friendlyURLMap = null;
 
 			Map<Locale, String> titleMap = new HashMap<>();
-			titleMap.put(LocaleUtil.US, titles[i]);
+			titleMap.put(LocaleUtil.getSiteDefault(), titles[i]);
 
 			Map<Locale, String> descriptionMap = new HashMap<>();
-			descriptionMap.put(LocaleUtil.US, descriptions[i]);
-
-			Map<Locale, String> contentMap = new HashMap<>();
-			contentMap.put(LocaleUtil.US, contents[i]);
+			descriptionMap.put(LocaleUtil.getSiteDefault(), descriptions[i]);
 
 			/*
-			 * This is a stupid hack, find a way to send structured content via the api
-			 * Maybe JournalConverter holds the magic
-			 * String content = _journalConverter.getContent(ddmStructure, fields);
-			 */ 
-			String content = "<?xml version=\"1.0\"?><root><dynamic-element name=\"content\" type=\"text_area\" index-type=\"text\" instance-id=\"qmln\"><dynamic-content language-id=\"en_US\"><![CDATA[<p>"
-					+ contents[i] + "</p>]]></dynamic-content></dynamic-element></root>";
+			 * Stolen directly from JournalDemoDataCreatorImplThis is a stupid hack, find a way to send structured content via the api
+			 * Maybe JournalConverter holds the magic String content =
+			 * _journalConverter.getContent(ddmStructure, fields);
+			 */
+			String content = _getStructuredContent(contents[i]);
 
 			/*
 			 * _jals.addArticle(userId, groupId, folderId, classNameId, classPK, articleId,
@@ -137,10 +156,56 @@ public class JournalArticleAdder {
 			 * articleURL, serviceContext);
 			 */
 
+			/*
+			 * List<JournalArticle> existingArticles = _jals.getArticles(groupId);
+			 * 
+			 * 
+			 * for (JournalArticle article:existingArticles) { if (titles[i] ==
+			 * article.getTitle()) {
+			 * 
+			 * double version = article.getVersion(); _jals.updateArticle(userId, groupId,
+			 * folderId, articleId, version, titleMap, descriptionMap, content,
+			 * "BASIC-WEB-CONTENT", serviceContext); } }
+			 */
 			_jals.addArticle(userId, groupId, folderId, titleMap, descriptionMap, content, "BASIC-WEB-CONTENT",
 					"BASIC-WEB-CONTENT", serviceContext);
 		}
 	}
+
+	private String _getStructuredContent(String content) {
+		Locale locale = LocaleUtil.getSiteDefault();
+
+		Document document = _createDocumentContent(locale.toString());
+
+		Element rootElement = document.getRootElement();
+
+		Element dynamicElementElement = rootElement.addElement(
+			"dynamic-element");
+
+		dynamicElementElement.addAttribute("index-type", "text");
+		dynamicElementElement.addAttribute("name", "content");
+		dynamicElementElement.addAttribute("type", "text_area");
+
+		Element element = dynamicElementElement.addElement("dynamic-content");
+
+		element.addAttribute("language-id", LocaleUtil.toLanguageId(locale));
+		element.addCDATA(content);
+
+		return document.asXML();
+	}
+
+	private Document _createDocumentContent(String locale) {
+		Document document = SAXReaderUtil.createDocument();
+
+		Element rootElement = document.addElement("root");
+
+		rootElement.addAttribute("available-locales", locale);
+		rootElement.addAttribute("default-locale", locale);
+
+		return document;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(JournalArticleAdder.class);
 
 	@Reference
 	private JournalArticleLocalService _jals;
@@ -165,6 +230,15 @@ public class JournalArticleAdder {
 
 	@Reference
 	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Reference
+	private AssetEntryLocalService _assetEntryLocalService;
+
+	@Reference
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
 
 	private volatile JournalArticleAdderConfiguration _journalArticleConfiguration;
 }
